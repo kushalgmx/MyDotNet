3e59ac45bbf6d3de87adafc5d0a0bb59bf0a9df5
Kushal <kushal@gmx.com>
2021-06-25 22:43:44 -0400

    add properties

--- MyCSharpClassLibrary1/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MyCSharpClassLibrary1
-{
-    public class Class1
-    {
-    }
-}
---
+++ MyCSharpClassLibrary1/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCSharpClassLibrary1
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public List<string>? Tags { get; set; }
+    }
+}
